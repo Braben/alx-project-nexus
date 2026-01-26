c371d7b9468de25c7164d66ebce7a38f2fcfb774
Benjamin Lamptey <kbenlamptey@gmail.com>
2026-01-26 17:47:50 +0000

    Footer added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/layout/Header";
 
+import Footer from "@/components/layout/Footer";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className="max-w-7xl mx-auto p-4">
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </main>
   );
 }
